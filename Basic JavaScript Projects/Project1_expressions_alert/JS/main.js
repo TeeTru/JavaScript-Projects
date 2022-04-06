bd86c9799d43a703e210d40ddd735e9c3c7f8e44
TeeTru <teranzbm@gmail.com>
2022-04-06 17:12:45 -0600

    pushing Basic JavaScript projects submission assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+window.alert("Hello, World!"); //This pops up a window stating whats quoted
+document.write('Hello,World!');//This displays on the website
+var A = "This is a string";//This is assigning a string to a variable
+document.write(A);// This displays the the variable
+var X = "Learning to code is AWESOME!!!";
+window.alert(X);
+document.write("Teran was very angry and screamed \"Why did you punch me!\""
++ " then without thought, Teran punched hime back.");
+var Z = "This is going to be" + " the greatest fight in history!"
+let result = Z.fontcolor("green");//Assigned a color using the fontcolor() method to variable Z
+document.write(result)
+var B = "Man", C = "Women", D = "Boy", E = "Girl";//THis is assigning multiple variables in one statement
+document.write(C + E);//This is concecating two variables
+document.write(4 + 5);//THis is an expression
+
+
