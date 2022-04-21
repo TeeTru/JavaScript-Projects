34bee5eb9e89c3c9ca3ce3d02a8631bb193b1dc1
TeeTru <teranzbm@gmail.com>
2022-04-21 13:06:22 -0600

    Tict tac toe push

--- JavaScript/JS/JavaScript_2.js
+++ JavaScript/JS/JavaScript_2.js
@@ -1,8 +1,49 @@
 //FORM VALIDATION
-function validationForm() {
+function validateForm() {
     let x = document.forms["myForm"]["fname"].value;
     if (x == "") {
         alert("Name must be filled out");
         return false;
     }
 }
+
+//FORM VALIDATION
+function validateForm() {
+    let x = document.forms["myForm"]["lname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+
+//FORM VALIDATION
+function validateForm() {
+    let x = document.forms["myForm"]["phonenumber"].value;
+    if (x == "") {
+        alert("Phone number must be filled out");
+        return false;
+    }
+}
+
+//Popup form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+//Automatic slideshow jQuery run after DOM is ready
+$(document).ready(function(){
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+    $('#slideshow > div:first')
+    .fadeout(1000)
+    .next()
+    .fadein(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
+});
--- JavaScript/JS/JavaScript_3.js
+++ JavaScript/JS/JavaScript_3.js
@@ -3,3 +3,7 @@ function displayType(capital) {
     var capitalType = capital.getAttribute("data-capital-type");
     alert(capitalType + " is the capital of " + capital.innerHTML + " !");
 }
+
+window.addEventListener('DOMContectLoaded', () => {
+    document.body.classList.remove('fade-out');
+});
